53414f4233fd9dc50141748a3bb807c9c5cce087
Manojkumar-12G <85819592+Manojkumar-12G@users.noreply.github.com>
2022-08-29 09:22:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Bootstrap 4 cdn link -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <!-- fontawesome link for new versions -->
+  <script src="https://kit.fontawesome.com/1c9ad4b785.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Home page</title>
+</head>
+<body>
+  <section class='title'>
+    <div class="container-fluid text-center first">
+      <h1>Welcome to Home page</h1>
+    </div>
+    <hr>
+  </section>
+
+   <section>
+     <div class="row">
+        <div class="col-4 m-auto">
+           <div class="card">
+             <div class="card-body text-center">
+
+          <a href="employee/index.php" class="btn btn-secondary" >Employee Module</a>
+           </div>
+         </div>
+      </div>
+      <div class="col-4 m-auto">
+        <div class="card">
+           <div class="card-body text-center">
+          <a href="seat/index.php" class="btn btn-info" >Seat Module</a>
+         </div>
+       </div>
+    </div>
+    </div>
+   </section>
+
+
+
+
+</body>
+</html>
